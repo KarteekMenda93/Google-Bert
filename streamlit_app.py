bb946711166f01bc94cbe7814f04566f61a5d9df
Karteek Menda <karteek.menda19@st.niituniversity.in>
2023-04-08 14:14:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
-st.title('🎈 Presentation App')
+st.title('Transformers and Bert')
 components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQfuY2EkxhqI7nHebJ2e9Kfc8YTlhyBeOUue540JgcOzuO3nFL7ZYfg19iv_1hhYd3TD-bh7TgVJhc6/embed?start=false&loop=false&delayms=3000", height=432)
